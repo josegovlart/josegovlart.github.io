d21389b1e26b69cd591384ee9d4e222398f5b533
jose-goulart <josevictor2000goulart@hotmail.com>
2022-08-24 16:07:29 -0300

    Create category's input

--- todo-list/src/App.vue
+++ todo-list/src/App.vue
@@ -11,7 +11,20 @@ const todos_asc = computed(() => todos.value.sort((a,b) => {
   return a.createdAt - b.createdAt;
 }))
 
-const addTodo = () => {};
+const addTodo = () => {
+  if (input_content.value.trim() === '' || input_category.value === null) {
+    return;
+  }
+
+  todos.value.push({
+    content: input_content.value,
+    category: input_category.value,
+    done: false,
+    createdAt: Date.getTime()
+  });
+};
+
+watch();
 
 watch(name, (newVal) => {
   localStorage.setItem('name', newVal)
@@ -36,9 +49,36 @@ onMounted(() => {
       
       <form @submit.prevent="addTodo">
         <h4>What's on your todo list?</h4>
-        <input type="text" placeholder="e.g. make a video" v-model="input_content">
+        <input 
+          type="text" 
+          placeholder="e.g. make a video" 
+          v-model="input_content">
 
-        {{ input_content }}
+        <h4>Pick a category</h4>
+
+        <div class="options">
+          <label>
+            <input 
+              type="radio" 
+              name="category"
+              value="business"
+              v-model="input_category" />
+            <span class="bubble business"></span>
+            <div>Business</div>
+          </label>
+
+          <label>
+            <input 
+              type="radio" 
+              name="category"
+              value="personal"
+              v-model="input_category" />
+            <span class="bubble personal"></span>
+            <div>Personal</div>
+          </label>
+        </div>
+
+        <input type="submit" value="Add todo">
       </form>
     </section>
   </main>
