25901187c200cf4dfb8b7c0b1db95e3ca61aa7dc
jose-goulart <josevictor2000goulart@hotmail.com>
2022-12-21 00:37:05 -0300

    Implement removeTodo function and finish project

--- todo-list/src/App.vue
+++ todo-list/src/App.vue
@@ -1,39 +1,52 @@
 <script setup>
-import { ref, onMounted, computed, watch } from 'vue'
+import { ref, onMounted, computed, watch } from "vue";
 
 const todos = ref([]);
-const name = ref('');
+const name = ref("");
 
-const input_content = ref('');
+const input_content = ref("");
 const input_category = ref(null);
 
-const todos_asc = computed(() => todos.value.sort((a,b) => {
-  return a.createdAt - b.createdAt;
-}))
+const todos_asc = computed(() =>
+  todos.value.sort((a, b) => {
+    return b.createdAt - a.createdAt;
+  })
+);
 
 const addTodo = () => {
-  if (input_content.value.trim() === '' || input_category.value === null) {
-    return;
-  }
+  if (input_content.value.trim() === "" || input_category.value === null) return;
 
   todos.value.push({
     content: input_content.value,
     category: input_category.value,
     done: false,
-    createdAt: Date.getTime()
+    createdAt: new Date().getTime(),
   });
+
+  todo_content.value = "";
+  todo_category.value = null;
 };
 
-watch();
+const removeTodo = (todo) => {
+  todos.value = todos.value.filter((t) => t !== todo);
+};
+
+watch(
+  todos,
+  (newVal) => {
+    localStorage.setItem("todos", JSON.stringify(newVal));
+  },
+  { deep: true }
+);
 
 watch(name, (newVal) => {
-  localStorage.setItem('name', newVal)
+  localStorage.setItem("name", newVal);
 });
 
 onMounted(() => {
-  name.value = localStorage.getItem('name') || '';
+  name.value = localStorage.getItem("name") || "";
+  todos.value = JSON.parse(localStorage.getItem("todos")) || [];
 });
-
 </script>
 
 <template>
@@ -46,40 +59,66 @@ onMounted(() => {
 
     <section class="create-todo">
       <h3>CREATE A TODO</h3>
-      
+
       <form @submit.prevent="addTodo">
         <h4>What's on your todo list?</h4>
-        <input 
-          type="text" 
-          placeholder="e.g. make a video" 
-          v-model="input_content">
+        <input
+          type="text"
+          placeholder="e.g. make a video"
+          v-model="input_content"
+        />
 
         <h4>Pick a category</h4>
 
         <div class="options">
           <label>
-            <input 
-              type="radio" 
+            <input
+              type="radio"
               name="category"
               value="business"
-              v-model="input_category" />
+              v-model="input_category"
+            />
             <span class="bubble business"></span>
             <div>Business</div>
           </label>
 
           <label>
-            <input 
-              type="radio" 
+            <input
+              type="radio"
               name="category"
               value="personal"
-              v-model="input_category" />
+              v-model="input_category"
+            />
             <span class="bubble personal"></span>
             <div>Personal</div>
           </label>
         </div>
 
-        <input type="submit" value="Add todo">
+        <input type="submit" value="Add todo" />
       </form>
+    </section>
+    <section class="todo-list">
+      <h3>TODO LIST</h3>
+      <div class="list">
+        <div
+          v-for="todo in todos_asc"
+          :class="`todo-item ${todo.done && 'done'}`">
+          <label>
+            <input type="checkbox" v-model="todo.done" />
+            <span :class="`bubble ${todo.category}`"></span>
+          </label>
+
+          <div class="todo-content">
+            <input type="text" v-model="todo.content" />
+          </div>
+
+          <div class="actions">
+            <button style="color: #313154;" @click="removeTodo(todo)">
+              <font-awesome-icon icon="fa-solid fa-trash" />
+            </button>
+          </div>
+        </div>
+      </div>
     </section>
   </main>
 </template>
--- todo-list/src/main.js
+++ todo-list/src/main.js
@@ -3,4 +3,11 @@ import './style.css'
 import App from './App.vue'
 import './main.css'
 
-createApp(App).mount('#app')
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { faTrash } from '@fortawesome/free-solid-svg-icons'
+
+library.add(faTrash);
+createApp(App)
+  .component('font-awesome-icon', FontAwesomeIcon)
+  .mount('#app');
