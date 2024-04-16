7cf7426facb9a3adfa153376604eb57abe7fc618
José Goulart <josegoulart@onsign.tv>
2024-04-16 13:42:04 -0300

    [webrtc] Show available devices on screen and also on console

--- webrtc/script.js
+++ webrtc/script.js
@@ -3,6 +3,21 @@ window.onload = () => {
 
   console.log('mediaDevices is available!');
 
+  const getAvailableDevices = () => {
+    const deviceList = document.getElementById("device-list");
+    navigator.mediaDevices.enumerateDevices()
+      .then((devices) => {
+        devices.forEach((device) => {
+          deviceList.innerHTML += `<li>${device.kind}: ${device.label} id = ${device.deviceId}</li>`;
+          console.log(`${device.kind}: ${device.label} id = ${device.deviceId}`);
+        });
+      })
+      .catch((err) => {
+        console.error(`${err.name}: ${err.message}`);
+      });
+
+  };
+
   const allow = (mic, cam) => {
     if (!mic && !cam) return;
 
@@ -21,6 +36,8 @@ window.onload = () => {
       });
   };
 
+  getAvailableDevices();
+  
   document.getElementById('cam').addEventListener("click", () => allow(false, true));
   document.getElementById('mic').addEventListener("click", () => allow(true, false));
   document.getElementById('miccam').addEventListener("click", () => allow(true, true));
