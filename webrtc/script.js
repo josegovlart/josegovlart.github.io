bdf779e5b386ffad08455610caca15d8f169e5ed
José Goulart <josegoulart@onsign.tv>
2024-04-16 17:51:06 -0300

    Create video element and audio waveform canva

--- webrtc/script.js
+++ webrtc/script.js
@@ -29,7 +29,72 @@ window.onload = () => {
       console.log('Trying to use camera.');
     }
 
-    navigator.mediaDevices.getUserMedia({audio:mic, video: cam})
+    const handleSuccess = (stream) => {
+      const audioContext = new AudioContext();
+      const source = audioContext.createMediaStreamSource(stream);
+      const analyser = audioContext.createAnalyser();
+      const canvas = document.getElementById('waveformCanvas');
+      const ctx = canvas.getContext('2d');
+
+      source.connect(analyser);
+
+      // Set up the drawing parameters
+      const bufferLength = analyser.frequencyBinCount;
+      const dataArray = new Uint8Array(bufferLength);
+
+      // Draw the waveform
+      function draw() {
+          const WIDTH = canvas.width;
+          const HEIGHT = canvas.height;
+
+          analyser.getByteTimeDomainData(dataArray);
+
+          ctx.fillStyle = 'rgb(200, 200, 200)';
+          ctx.fillRect(0, 0, WIDTH, HEIGHT);
+
+          ctx.lineWidth = 2;
+          ctx.strokeStyle = 'rgb(0, 0, 0)';
+
+          ctx.beginPath();
+
+          const sliceWidth = WIDTH * 1.0 / bufferLength;
+          let x = 0;
+
+          for(let i = 0; i < bufferLength; i++) {
+              const v = dataArray[i] / 128.0;
+              const y = v * HEIGHT/2;
+
+              if(i === 0) {
+                  ctx.moveTo(x, y);
+              } else {
+                  ctx.lineTo(x, y);
+              }
+
+              x += sliceWidth;
+          }
+
+          ctx.lineTo(canvas.width, canvas.height/2);
+          ctx.stroke();
+
+          requestAnimationFrame(draw);
+      }
+
+      draw();
+    }
+
+    const constraints = { audio:mic, video: cam };
+    navigator.mediaDevices.getUserMedia(constraints)
+      .then((stream) => {
+        if (constraints.video) {
+          const video = document.getElementById('videoElement');
+          video.srcObject = stream
+        }
+
+        if (constraints.audio) {
+          handleSuccess(stream);
+        }
+        console.log('Success on using getUserMedia', constraints);
+      })
       .catch(err => {
         console.log('error on accessing mic:' + mic + ' and cam: ' + cam);
         console.error(err);
